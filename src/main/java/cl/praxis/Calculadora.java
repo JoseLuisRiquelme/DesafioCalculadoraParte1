91c428df56900d12d6c329017e04535124ee40be
Jose Luis Riquelme <jlrsaez@gmail.com>
2024-06-12 20:28:52 -0400

    Adding Classes and Tests

---
+++ src/main/java/cl/praxis/Calculadora.java
@@ -0,0 +1,52 @@
+package cl.praxis;
+
+public class Calculadora {
+
+    private int numero1;
+    private int numero2;
+
+    public Calculadora() {
+
+    }
+
+    public Calculadora(int numero1, int numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public int restar(int numero1, int numero2){
+        int resultado = numero1-numero2;
+        return resultado;
+    };
+
+    public int sumar(int numero1,int numero2){
+        int resultado = numero1+numero2;
+        return resultado;
+    };
+
+    public int multiplicar(int numero1,int numero2){
+        int resultado = numero1*numero2;
+        return resultado;
+    };
+
+    public int dividir(int numero1,int numero2){
+        int resultado = numero1/numero2;
+        return resultado;
+    };
+
+    public int getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(int numero1) {
+        this.numero1 = numero1;
+    }
+
+    public int getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(int numero2) {
+        this.numero2 = numero2;
+    }
+}
